de0c3db9e6419ee316482d77c9312561987fcdc6
unknown <beckywgoodflair@github.com>
2022-07-09 01:23:42 -0400

    Clean slate with canvas element dimensions set by index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
-function msg(){  
- alert("Hello Javatpoint");  
-}  
+const canvas = document.querySelector('canvas');
+
+const gameContext = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+gameContext.fillRect(0, 0, canvas.width, canvas.height);
+
+
+function OnClickBaby(msg) {
+   
+    var pTag = document.querySelector('p');
+
+    pTag.append(msg);
+}
