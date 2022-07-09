6861abdccc20c7544afb070295bd178b24fe8337
Waspy <beckywgoodflair@github.com>
2022-07-09 15:22:37 -0400

    Starting place for all participants

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,3 @@ canvas.width = 1024;
 canvas.height = 576;
 
 gameContext.fillRect(250, 0, canvas.width, canvas.height);
-
-
-
-function OnClickBaby(textToAppend) {
-   
-    const pTag = document.querySelector('p');
-
-    pTag.append(textToAppend);
-
-    linebreak = document.createElement("br");
-    pTag.appendChild(linebreak);
-}
-
-function OnClickClear() {
-
-    const pTag = document.querySelector('p');
-    pTag.innerHTML = ""; 
-}
